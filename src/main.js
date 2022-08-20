ba9e8682241e04e8e9596d18e4dfe4bdb1172809
herisusantoarismann <heri.s.arisman@gmail.com>
2022-08-20 12:45:03 +0700

    Add dashboard page

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import "./index.css";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+library.add(faPlus);
 
-createApp(App).mount("#app");
+createApp(App).component("font-awesome-icon", FontAwesomeIcon).mount("#app");
