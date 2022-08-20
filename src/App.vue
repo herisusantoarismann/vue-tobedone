3b560faa873409f43bf6a8232b448889f8561a96
herisusantoarismann <heri.s.arisman@gmail.com>
2022-08-20 12:08:50 +0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="min-h-screen w-screen bg-secondary">
+    <div class="bg-primary px-4 py-3 text-white uppercase font-semibold">
+      <h1 class="text-xl">To Do List App</h1>
+    </div>
+    <div class="p-4">
+      <h2 class="font-semibold text-lg">Activity</h2>
+      <button>+</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+};
+</script>
+
+<style></style>
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: { primary: "#16ABF8", secondary: "#F4F4F4" },
+    },
+  },
+  plugins: [],
+};
